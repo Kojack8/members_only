f1fd0b0d75455acfc62c410268891dc820b3e139
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-02-26 16:42:52 -0800

    added devise migration

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,13 @@
 class PostsController < ApplicationController
-  before_action :require_login, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
   
   def index 
     @posts = Post.all
   end
 
-  private
-
-  def require_login
-    unless logged_in?
-      flash[:error] = "You must be logged in to access this section"
-      redirect_to new_login_url # halts request cycle
-    end
+  def new
+    @posts = Post.new
   end
+
+  
 end
