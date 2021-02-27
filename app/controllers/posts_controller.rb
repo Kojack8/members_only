8251679c6cf41cdc25a191ea920296cba205b92f
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-02-26 16:21:46 -0800

    Added require_login method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,16 @@
 class PostsController < ApplicationController
+  before_action :require_login, only: [:new, :create]
+  
   def index 
     @posts = Post.all
+  end
+
+  private
+
+  def require_login
+    unless logged_in?
+      flash[:error] = "You must be logged in to access this section"
+      redirect_to new_login_url # halts request cycle
+    end
   end
 end
