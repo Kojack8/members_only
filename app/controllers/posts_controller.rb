707fd6a93b4b76bc663027aeddede37cef905cad
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-03-02 16:59:47 -0800

    Added user (foreign_id) column to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,27 @@ class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   
   def index 
-    @posts = Post.all
+    @post = Post.all
   end
 
   def new
-    @posts = Post.new
+    @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to :index
+    else
+      render :new
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:body, :user_id)
+    end
 
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_27_002910) do
+ActiveRecord::Schema.define(version: 2021_03_03_005910) do
 
   create_table "posts", force: :cascade do |t|
     t.text "body"
-    t.integer "author_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["author_id"], name: "index_posts_on_author_id"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -32,5 +32,5 @@ ActiveRecord::Schema.define(version: 2021_02_27_002910) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "posts", "authors"
+  add_foreign_key "posts", "users"
 end
