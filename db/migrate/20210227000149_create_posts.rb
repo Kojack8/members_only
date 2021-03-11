b1528605c0396f93e69ab71f460fcd21709361d8
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-03-11 12:14:11 -0600

    revert previous change

--- db/migrate/20210227000149_create_posts.rb
+++ db/migrate/20210227000149_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.text :body
-
+      t.references :author, null: false, foreign_key: true
 
       t.timestamps
     end
